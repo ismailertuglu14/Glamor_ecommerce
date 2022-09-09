dcf668723857b755d2497a97b32d6bf5426006e5
ismailertuglu <ismailertgl@gmail.com>
2022-09-09 15:02:37 +0300

    route changes

--- lib/core/init/navigation/navigation_route.dart
+++ lib/core/init/navigation/navigation_route.dart
@@ -1,4 +1,5 @@
 import 'package:client/core/constants/navigation/navigation_constants.dart';
+import 'package:client/view/authenticate/login/view/login_test.dart';
 import 'package:client/view/authenticate/login/view/login_view.dart';
 import 'package:client/view/authenticate/splash/view/splash_view.dart';
 import 'package:flutter/material.dart';
@@ -11,10 +12,12 @@ class NavigationRoute {
   Route<dynamic> generateRoute(RouteSettings args) {
     switch (args.name) {
       case NavigationConstants.DEFAULT:
-        return normalNavigate(const LoginView(), NavigationConstants.DEFAULT);
+        return normalNavigate(const SplashView(), NavigationConstants.DEFAULT);
       case NavigationConstants.LOGIN_VIEW:
         return normalNavigate(
             const LoginView(), NavigationConstants.LOGIN_VIEW);
+      case '/test':
+        return animatedNavigate(LoginTest());
 
       default:
         throw Exception('Screen doesn\'t exist!');
@@ -29,5 +32,24 @@ class NavigationRoute {
         builder: (context) => widget,
         //analytciste görülecek olan sayfa ismi için pageName veriyoruz
         settings: RouteSettings(name: pageName));
+  }
+
+  PageRouteBuilder animatedNavigate(Widget widget) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => widget,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(0.0, 1.0);
+        const end = Offset.zero;
+        const curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
   }
 }
