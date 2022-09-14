ef921eed23cd7ee247cec90d5ee77bfed679a8b0
ismailertuglu <ismailertgl@gmail.com>
2022-09-14 23:41:37 +0300

    model changes

--- lib/product/models/user_model.dart
+++ lib/product/models/user_model.dart
@@ -1,18 +1,24 @@
-class UserModel {
-  final String username;
-  final String email;
-  final int type;
+import 'package:client/core/base/base_model.dart';
+
+class UserModel extends IBaseModel<UserModel> {
+  late String username;
+  late String email;
+  late int type;
 
   UserModel({required this.username, required this.email, required this.type});
 
-  factory UserModel.fromJson(Map<String, dynamic> json) {
-    return UserModel(
-      username: json['username'],
-      email: json['email'],
-      type: json['type'] as int,
-    );
+  UserModel.fromJson(Map<String, dynamic> json) {
+    username = json['username'];
+    email = json['email'];
+    type = json['type'] as int;
   }
 
+  @override
+  UserModel fromJson(Map<String, dynamic> json) {
+    return UserModel.fromJson(json);
+  }
+
+  @override
   Map<String, dynamic> toJson() => {
         "username": username,
         "email": email,
