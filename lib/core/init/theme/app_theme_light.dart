11de6ea014795e10feb9732e8ee0e92695359b7a
ismailertuglu <ismailertgl@gmail.com>
2022-09-16 22:03:35 +0300

    color scheme changes

--- lib/core/init/theme/app_theme_light.dart
+++ lib/core/init/theme/app_theme_light.dart
@@ -62,7 +62,7 @@ class AppThemeLight extends AppTheme with ILightTheme {
   ColorScheme get _appColorScheme {
     return ColorScheme(
       primary: colorSchemeLight!.black,
-      primaryVariant: Colors.white, //xx
+      primaryVariant: Colors.grey, //xx
       secondary: Colors.green,
       secondaryVariant: colorSchemeLight!.azure,
       surface: Colors.blue, //xx
