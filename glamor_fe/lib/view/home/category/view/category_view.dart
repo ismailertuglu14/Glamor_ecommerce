b48f35316d70821429afb13af69356a74cb8a516
ismailertuglu <ismailertgl@gmail.com>
2022-09-24 23:13:27 +0300

    category cubit created

--- glamor_fe/lib/view/home/category/view/category_view.dart
+++ glamor_fe/lib/view/home/category/view/category_view.dart
@@ -1,4 +1,6 @@
+import 'package:client/view/home/feed/cubit/products_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CategoryView extends StatefulWidget {
   const CategoryView({super.key});
@@ -10,13 +12,17 @@ class _CategoryViewState extends State<CategoryView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: dummyData.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: dummyData[index]['icon'],
-            title: dummyData[index]['title'],
-            trailing: const Icon(Icons.keyboard_arrow_right_rounded),
+      body: BlocBuilder<ProductsCubit, ProductsState>(
+        builder: (context, state) {
+          return ListView.builder(
+            itemCount: state.categories?.length ?? 0,
+            itemBuilder: (context, index) {
+              return ListTile(
+                //leading: dummyData[index]['icon'],
+                title: Text(state.categories?[index] ?? ''),
+                trailing: const Icon(Icons.keyboard_arrow_right_rounded),
+              );
+            },
           );
         },
       ),
