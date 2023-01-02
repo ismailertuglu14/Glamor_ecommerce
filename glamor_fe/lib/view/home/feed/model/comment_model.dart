600fb58b15680421ab281c7272515cc97a34eb54
İsmail Ertuğlu <ismailertgl@gmail.com>
2023-01-02 17:41:23 +0300

    unnecessary method removed

--- glamor_fe/lib/view/home/feed/model/comment_model.dart
+++ glamor_fe/lib/view/home/feed/model/comment_model.dart
@@ -4,13 +4,16 @@ class Comment extends INetworkModel<Comment> {
   int? id;
   String? message;
   List<Comment>? replies;
+
   Comment({this.id, this.message, this.replies});
-  factory Comment.fromJson(Map<String, dynamic> json) {
-    return Comment();
-  }
+
   @override
   Comment fromJson(Map<String, dynamic> json) {
-    return Comment.fromJson(json);
+    return Comment(
+      id: json['id'] ?? 0,
+      message: json['message'] ?? '',
+      replies: json['replies'] ?? [],
+    );
   }
 
   @override
