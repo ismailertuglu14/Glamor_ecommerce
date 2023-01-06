c4f16be096a8c02005937433e92210e2471d47e9
İsmail Ertuğlu <ismailertgl@gmail.com>
2023-01-06 23:41:19 +0300

    subcategory changes.

--- glamor_fe/lib/view/home/category/models/subcategory_model.dart
+++ glamor_fe/lib/view/home/category/models/subcategory_model.dart
@@ -2,47 +2,21 @@ import 'package:vexana/vexana.dart';
 
 class Subcategory extends INetworkModel<Subcategory> {
   Subcategory({
-    this.title,
-    this.subcategories,
-  });
-  String? title;
-  List<Subcategories>? subcategories;
-
-  Subcategory.fromJson(Map<String, dynamic> json) {
-    title = json['title'] ?? '';
-    subcategories = List.from(json['subcategories'])
-        .map((e) => Subcategories.fromJson(e))
-        .toList();
-  }
-
-  @override
-  Map<String, dynamic> toJson() {
-    throw UnimplementedError();
-  }
-
-  @override
-  Subcategory fromJson(Map<String, dynamic> json) {
-    return Subcategory.fromJson(json);
-  }
-}
-
-class Subcategories {
-  Subcategories({
     this.id,
     this.title,
   });
+
   int? id;
   String? title;
+  @override
+  Subcategory fromJson(Map<String, dynamic> json) => Subcategory(
+        id: json["id"] == null ? null : json["id"],
+        title: json["title"] == null ? null : json["title"],
+      );
 
-  Subcategories.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    title = json['title'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final data = <String, dynamic>{};
-    data['id'] = id;
-    data['title'] = title;
-    return data;
-  }
+  @override
+  Map<String, dynamic> toJson() => {
+        "id": id == null ? null : id,
+        "title": title == null ? null : title,
+      };
 }
