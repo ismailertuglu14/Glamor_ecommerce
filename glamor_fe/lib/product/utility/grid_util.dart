f3f5083363c36cc84485cd2213849ccb8d5f9226
İsmail Ertuğlu <ismailertgl@gmail.com>
2023-01-06 23:40:45 +0300

    grid pixel changed.

--- glamor_fe/lib/product/utility/grid_util.dart
+++ glamor_fe/lib/product/utility/grid_util.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 class FeedGridDelegate extends SliverGridDelegateWithFixedCrossAxisCount {
   FeedGridDelegate({required double height, required double width})
       : super(
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 0,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
           crossAxisCount: 2,
           childAspectRatio: (width / height),
         );
